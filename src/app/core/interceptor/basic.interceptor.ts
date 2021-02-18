fa8c37a3e07b2d2ba71afc44978b5ce31e5292db
Grzegorz <grzegorz.machynia1@gmail.com>
2021-02-18 17:50:59 +0100

    Add basic interceptor

---
+++ src/app/core/interceptor/basic.interceptor.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class BasicInterceptor implements HttpInterceptor {
+
+  constructor() {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    request = request.clone({
+      setHeaders: {
+        'Content-Type': 'application/json; charset=utf8'
+      }
+  });
+    return next.handle(request);
+  }
+}
