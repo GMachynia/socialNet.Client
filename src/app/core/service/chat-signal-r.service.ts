1acc1b476dae135b0609141b35dfc00236320bb6
Grzegorz <grzegorz.machynia1@gmail.com>
2021-02-22 17:03:13 +0100

    SignalR changes

--- src/app/core/service/chat-signal-r.service.ts
+++ src/app/core/service/chat-signal-r.service.ts
@@ -1,23 +1,44 @@
 import { EventEmitter, Injectable } from '@angular/core';
 import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
 import { environment } from '@env/*';
+import { AuthService } from './auth.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatSignalRService {
 
-  public messageReceived = new EventEmitter<any>();
-  public connectionEstablished = new EventEmitter<Boolean>();
+  private messageReceived = new EventEmitter<any>();
+  private messageReceivedFromUser = new EventEmitter<any>();
+  private connectionEstablished = new EventEmitter<Boolean>();
   private _hubConnection: HubConnection;
 
-  constructor() {
+  constructor(private _authService: AuthService) {
     this.startConnection();
     this.receiveMessage();
    }
+  
+  public getMessage(): Observable<any>{
+    return this.messageReceived.asObservable();
+  }
+
+  public getMessageFromTo(connectionIdFrom: string, connectionIdTo: string): Observable<any>{
+    return this.messageReceivedFromUser.asObservable();
+  }
 
   public sendToUser = (connectionId: string, message: string): void => {
     this._hubConnection.invoke('SendToUser', connectionId, message);
+  }
+
+  private responseFromUser = (): void => {
+    this._hubConnection.on('ReceiveMessageFromUser', (data: any) => {
+      this.messageReceived.emit(data);
+    });
+  }
+
+  public sendToAll = (message: string): void => {
+    this._hubConnection.invoke('SendToAll', message);
   }
 
   public createGroup = (groupName: string): void => {
@@ -33,15 +54,20 @@ export class ChatSignalRService {
   }
 
   private receiveMessage = (): void => {
-    this._hubConnection.on('ReceiveMessage', (data: any) => {
+    this._hubConnection.on('ReceiveMessageToAll', (data: any) => {
       this.messageReceived.emit(data);
     });
   }
 
   private startConnection = () => {
     this._hubConnection = new HubConnectionBuilder()
-                            .withUrl(`${environment.apiUrl}/signalR/chat`)
-                            .build();                      
+                            .withUrl(`${environment.apiUrl}/signalR/chat`,
+                            {
+                              accessTokenFactory: ()=> this._authService.currentUserTokenValue
+                            }
+                            )
+                            .build();     
+               
     this._hubConnection
       .start()
       .then(() => console.log('SignalR Chat: Connection started'))
--- src/app/core/service/notification-signal-r.service.ts
+++ src/app/core/service/notification-signal-r.service.ts
@@ -1,9 +1,32 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { environment } from '@env/*';
+import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationSignalRService {
 
-  constructor() { }
+  public messageReceived = new EventEmitter<any>();
+  public connectionEstablished = new EventEmitter<Boolean>();
+  private _hubConnection: HubConnection;
+
+  constructor() {
+    this.startConnection();
+   }
+
+  private startConnection = () => {
+    this._hubConnection = new HubConnectionBuilder()
+                            .withUrl(`${environment.apiUrl}/signalR/notification`)
+                            .build();                      
+    this._hubConnection
+      .start()
+      .then(() => console.log('SignalR Notification: Connection started'))
+      .catch(err => console.log('SignalR Notification: Error while starting connection: ' + err))
+  }
+
+  public stopConnection(){
+    this._hubConnection.stop();
+  }
+
 }
