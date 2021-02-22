cd583eb5fe8615f878b2c177e0f8575fffdd0d4c
Grzegorz <grzegorz.machynia1@gmail.com>
2021-02-22 17:01:27 +0100

    Auth service changes

--- src/app/core/service/auth.service.ts
+++ src/app/core/service/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { environment } from '@env/*';
 import { BehaviorSubject, Observable, of } from 'rxjs';
 import { tap } from 'rxjs/operators';
@@ -21,14 +22,17 @@ export class AuthService {
   private currentUserSubject: BehaviorSubject<IUser>;
   public currentUser: Observable<IUser>;
 
-  constructor(private _httpClient: HttpClient) { 
+  constructor(
+    private _httpClient: HttpClient,
+    private _router: Router) { 
     this.currentUserSubject = new BehaviorSubject<IUser>(JSON.parse(localStorage.getItem('currentUser')));
     this.currentUser = this.currentUserSubject.asObservable();
   }
 
   public login(credentials: IUserCredentials) {
     return this._httpClient.post<any>(`${environment.apiUrl}/api/User/login`, credentials)
-      .pipe(tap(user => {
+      .pipe(
+        tap(user => {
         localStorage.setItem('currentUser', JSON.stringify(user));
         this.currentUserSubject.next(user);
         return user;
@@ -38,9 +42,18 @@ export class AuthService {
   public logout() {
     localStorage.removeItem('currentUser');
     this.currentUserSubject.next(null);
+    this._router.navigate(['/auth/login']);
   }
 
   public get currentUserValue(): IUser {
     return this.currentUserSubject.value;
+  }
+
+  public get currentUsernameValue(): string {
+    return this.currentUserSubject.value.username;
+  }
+
+  public get currentUserTokenValue(): string {
+    return this.currentUserSubject.value.token;
   }
 }
